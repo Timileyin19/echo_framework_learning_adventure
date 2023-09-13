72e3209ff34c9f50f9556678fb7e21ec805a69cb
timileyin19 <abioduntimileyin19@gmail.com>
2023-09-13 18:57:50 +0200

    file path for server

--- config/setenv.go
+++ config/setenv.go
@@ -1,5 +1,25 @@
 package config
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
 // set file name
 // var EnVar = GetConfig("env")					// if runing locally
-var EnVar = GetConfig("production")
+
+
+func getConfigFile() string {
+	// RUNNING ON THE SERVER
+	path, _ := os.Executable()
+	
+	filePath := filepath.Dir(path)
+	
+	configFile := fmt.Sprintf("%v/config/production.yaml", filePath)
+
+	return configFile
+}
+var configPath = getConfigFile()
+
+var EnVar = GetConfig(configPath)
